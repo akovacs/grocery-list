3cb7c08cc2d8d691b14c2e6f02249eefd8807a2f
Arpad Kovacs <minhmeoke@gmail.com>
2019-02-16 23:02:18 -0800

    Upload files to google cloud

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Instructions to Run:
 
+    pip install flask
+    pip install google-cloud-storage
     export FLASK_APP=server.py
+    export GOOGLE_APPLICATION_CREDENTIALS=".json"
     python -m flask run --host=0.0.0.0
--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 import os
+import datetime
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
+from google.cloud import storage
 
 
 UPLOAD_FOLDER = '/home/akovacs/akproject/grocery-list/uploads'
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
+PROJECT_ID = '315041698645'
+BUCKET_NAME = 'grocerylistyum'
 
 app = Flask(__name__)
 
@@ -35,7 +39,11 @@ def index():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
+            # Save local copy of the file
             file.save(os.path.join(UPLOAD_FOLDER, filename))
+            # Upload to google cloud
+            uploaded_url = upload_file(file.read(), file.filename, file.content_type)
+            print(uploaded_url)
             return redirect(url_for('index',
                 filename=filename))
 
@@ -43,4 +51,39 @@ def index():
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def _safe_filename(filename):
+    """
+    Generates a safe filename that is unlikely to collide with existing objects
+    in Google Cloud Storage.
+    ``filename.ext`` is transformed into ``filename-YYYY-MM-DD-HHMMSS.ext``
+    """
+    filename = secure_filename(filename)
+    date = datetime.datetime.utcnow().strftime("%Y-%m-%d-%H%M%S")
+    basename, extension = filename.rsplit('.', 1)
+    return "{0}-{1}.{2}".format(basename, date, extension)
+
+
+def upload_file(file_stream, filename, content_type):
+    """
+    Uploads a file to a given Cloud Storage bucket and returns the public url
+    to the new object.
+    """
+    filename = _safe_filename(filename)
+
+    client = storage.Client(project=PROJECT_ID)
+    bucket = client.bucket(BUCKET_NAME)
+    blob = bucket.blob(filename)
+
+    blob.upload_from_string(
+        file_stream,
+        content_type=content_type)
+
+    url = blob.public_url
+
+    if isinstance(url, six.binary_type):
+        url = url.decode('utf-8')
+
+    return url
 
